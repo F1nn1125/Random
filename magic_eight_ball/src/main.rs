f2ed84004e4d7c8d1cdb8cb9de8d5e840f468ce7
Finn <71734783+F1nn1125@users.noreply.github.com>
2024-07-24 13:05:17 +1200

    Added all answers and made more user-friendly

--- magic_eight_ball/src/main.rs
+++ magic_eight_ball/src/main.rs
@@ -2,7 +2,9 @@ use rand::Rng;
 use std::io;
 
 fn main() {
-    let possible_answers: [&str; 3] = ["yes", "no", "maybe"];
+    println!("What is your question? ");
+
+    let possible_answers: [&str; 20] = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes Signs point to yes", "Reply hazy", "Try again", "Ask again later", "Better not tell you that", "Cannot predict now", "Concentrate and ask again", "Dont count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
     
     let answer_index: usize = rand::thread_rng().gen_range(0..possible_answers.len());
     let answer: &str = possible_answers[answer_index];
@@ -13,6 +15,8 @@ fn main() {
         .expect("Failed to read line");
 
     println!("{answer}");
+
+    println!("\nPress any key to exit");
 
     io::stdin()
         .read_line(&mut question)
